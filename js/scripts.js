91574bb56a720c6033d867a299a00dc50bfb46eb
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-10-25 08:20:50 -0700

    recommit

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
-var c=0;
-var r=0;
-var p=0;
-var j=0;
+
+function countAnswers(qq1, qq2, qq3, qq) {
+  var c= 0;
+  var r= 0;
+  var p= 0;
+  var j=  ;
+  var answersARR = [c, r, p, j];
+  var topResult = answersArr.sort();
 
 
-function countAnswers(qq1, qq2, qq3, qq4,){
   // question 1 logic
   if (qq1 == "1") {
     j++;
     p++;
-  } else if (qq1 == "2") {
+  } else if (qq1 === "2") {
     c++;
     r++;
-  } else if (qq1 == "3") {
+  } else if (qq1 === "3") {
     c++;
     r++;
     j++;
     p++;
-  };
+  }
   // question 2 logic
   if (qq2 == "1") {
     p++;
@@ -31,7 +34,7 @@ function countAnswers(qq1, qq2, qq3, qq4,){
   } else if (qq2 == "4") {
     j++;
     r++;
-  };
+  }
   // question 3 logic
   if (qq3 =="yes") {
     r++;
@@ -40,7 +43,7 @@ function countAnswers(qq1, qq2, qq3, qq4,){
     c++;
     j++;
     p++;
-  };
+  }
   // question 4 logic Below
   if (qq4 == "a") {
     j++;
@@ -55,80 +58,44 @@ function countAnswers(qq1, qq2, qq3, qq4,){
   } else if (qq4 == "d") {
     j++;
   }
-
+  return topResult
+  console.log(topResult)
 };
+
+
 $(document).ready(function() {
 
-  console.log(name);
-
-  $("#formOne").submit(function(event){
-    var name =$("#nme").val();
+  $("#form1").submit(function(event){
+    event.preventDefault();
+    var qq1 =$("#q1").val();
+    var qq2 =$("#q2").val();
+    var qq3 =$("#q3").val();
+    var qq4 =$("#q4").val();
+    var results = countAnswers(qq1,qq2,qq3,qq4);
+    console.log(results)
   });
-    // var qq1 =$('#q1').val();
-    // var qq2 =$('#q2').val();
-    // var qq3 =$('#q3').val();
-    // var qq4 =$('#q4').val();
-    // countAnswers(qq1,qq2,qq3,qq4);
-    // var answersARR = [c, r, p, j];
-    // answersARR.sort();
-    // var topResult = answersARR[0];
-    
-      $('button').click(function(){
 
-        if(this.id =='begin'){
-          $('#greeting').hide();
-          $('#ID').show();
-        }
-
-        if(this.id == 'continue'){
-          $('#ID').hide();
-          $('#brain').show();
-        }
-
-        if(this.id == 'go_on'){
-          $('#brain').hide();
-          $('#work').show();
-        }
-
-        if(this.id == 'next1'){
-          $("#work").hide();
-          $("#prsntpe").show();
-        }
-
-        if(this.id == 'next2'){
-          $('#prsntpe').hide();
-          $('#why').show();
-        }
-
-        if(this.id == 'next3') {
-          $('#why').hide();
-          $('#result').fadeIn();
-        }
-      });
-      $("#formOne").submit(function(event){
-        event.preventDefault();
-        var name =$("#nme").val();
-        console.log(name);
-      });
-      $("#formTwo").submit(function(event){
-        event.preventDefault();
-        var qq1 =$("#q1").val();
-        console.log(qq1);
-      });
-      $("#formThree").submit(function(event){
-        event.preventDefault();
-        var qq2 =$("#q2").val();
-        console.log(qq2);
-      });
-      $("#formFour").submit(function(event){
-        event.preventDefault();
-        var qq3 =$("#q3").val();
-        console.log(qq3);
-      });
-      $("#formFive").submit(function(event){
-        event.preventDefault();
-        var qq4 =$("#q4").val();
-        console.log(qq4);
-      });
-
+    $('button').click(function(){
+      if(this.id =='begin'){
+        $('#greeting').hide();
+        $('#formOne').show();
+      }
     });
+    $('button').click(function(){
+      if (this.id =='end') {
+        $("#pt1").hide();
+      }
+    });
+
+
+
+
+
+
+
+  });
+
+
+  // answersARR.sort();
+  // var topResult = answersARR[0];
+  // console.log(name);
